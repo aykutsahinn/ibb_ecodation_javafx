0fed1c4aa3a260d725eb74e71d0272256f81c2ef
aykutsahinn <sahinaykut97@gmail.com>
2025-03-22 20:25:55 +0300

    LoginController

---
+++ src/main/java/com/aykutsahin/ibb_ecodation_javafx/dao/IDaoImplements.java
@@ -0,0 +1,16 @@
+package com.aykutsahin.ibb_ecodation_javafx.dao;
+
+import com.aykutsahin.ibb_ecodation_javafx.database.SingletonDBConnection;
+
+import java.sql.Connection;
+
+public interface IDaoImplements<T> extends ICrud<T>,IGenericsMethod<T>,ILogin<T>  {
+    // Gövdeli Method
+    default Connection iDaoImplementsDatabaseConnection() {
+        // Singleton DB
+        return SingletonDBConnection.getInstance().getConnection();
+
+        // Singleton Config
+        //return SingletonPropertiesDBConnection.getInstance().getConnection();
+    }
+}
